f0e3d865c783d84c2da3d25f61b4b77bee32deb1
Xinyu Z <syuxxxsp@163.com>
2024-01-19 07:07:50 +0000

    added debug Tag

--- GeoQuiz/app/src/main/java/xinyuz0416/android/geoquiz/MainActivity.kt
+++ GeoQuiz/app/src/main/java/xinyuz0416/android/geoquiz/MainActivity.kt
@@ -2,9 +2,12 @@ package xinyuz0416.android.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import xinyuz0416.android.geoquiz.databinding.ActivityMainBinding
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     //    use viewBinding
@@ -24,6 +27,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG,"onCreate(Bundle?) called")
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -67,5 +71,30 @@ class MainActivity : AppCompatActivity() {
         val messageResId = if(userAnswer == correctAnswer) R.string.correct_toast else R.string.incorrect_toast
         Toast.makeText(this,messageResId, Toast.LENGTH_SHORT).show()
 
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause(){
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
     }
 }
