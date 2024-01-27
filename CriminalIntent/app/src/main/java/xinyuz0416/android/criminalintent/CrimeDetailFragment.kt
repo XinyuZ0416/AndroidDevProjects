0bf75c4097143430c372655d038d0ba9a3b600f8
Xinyu Z <syuxxxsp@163.com>
2024-01-27 17:29:55 +0000

    upload CriminalIntent

---
+++ CriminalIntent/app/src/main/java/xinyuz0416/android/criminalintent/CrimeDetailFragment.kt
@@ -0,0 +1,66 @@
+package xinyuz0416.android.criminalintent
+
+import android.os.Bundle
+import android.view.*
+import androidx.core.widget.*
+import androidx.fragment.app.*
+import xinyuz0416.android.criminalintent.databinding.*
+import java.util.*
+
+// present the details of a specific crime,  update those details as the user changes them.
+class CrimeDetailFragment: Fragment() {
+
+    private lateinit var crime: Crime
+    private var _binding: FragmentCrimeDetailBinding? = null
+    private val binding
+        get() = checkNotNull(_binding){
+            "Cannot access binding because it is null. Is the view visible?"
+        }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        crime = Crime(
+            id = UUID.randomUUID(),
+            title = "",
+            date = Date(),
+            isSolved = false
+        )
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentCrimeDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            crimeTitle.doOnTextChanged { text, _, _, _ ->
+                crime = crime.copy(title = text.toString())
+            }
+
+            crimeDate.apply {
+                text = crime.date.toString()
+                isEnabled = false
+            }
+
+            crimeSolved.setOnCheckedChangeListener { _, isChecked ->
+                crime = crime.copy(isSolved = isChecked)
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+}
