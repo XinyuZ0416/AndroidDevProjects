3c235f7049244015e448c218106c8b3abe36c8f6
Xinyu Z <syuxxxsp@163.com>
2024-01-31 16:05:49 +0000

    implemented crime list fragment and layout, implemented crime item layout

--- CriminalIntent/app/build.gradle.kts
+++ CriminalIntent/app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
